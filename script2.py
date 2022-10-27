1e79947c47ebaad330e8fb44918e95ec3abc9fe9
RooneSE <xyjiugah1991@gmail.com>
2022-10-27 15:51:51 +0300

    'dsda'

---
+++ script2.py
@@ -0,0 +1,27 @@
+a = int(input())
+b = int(input())
+
+print(f'Сумма числе a & b = {a + b}')
+print(f'Разность чисел a & b = {a - b}')
+print(f'Произведение числе a & b = {a * b}')
+
+
+x = abs(int(input()))
+y = abs(int(input()))
+
+print(int((x - y) / 1 + (x * y)))
+
+
+dlina_rebra = int(input())
+V = dlina_rebra ** 3
+S = 4 * dlina_rebra ** 2
+
+print(f'площадь боковой поверхности  куба равняется = {S}')
+print(f'объем куба равняется = {V}')
+
+kat1 = int(input())
+kat2 = int(input())
+
+print(f'гипотенуза равняется = {int((kat1 ** 2)) + int((kat1 ** 2)) * 0.5}')
+print(f'площадь прямоугольная треугольника равняется = {int(kat1 * kat2 / 2)}')
+
